db72b9737daf867967af923993cba44f5815d7e3
Tuan Nguyen <ntuan.2502@gmail.com>
2025-07-01 14:39:26 +0700

    feat(prisma): integrate Prisma with PostgreSQL and create User model

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit(): Promise<void> {
+    await (this.$connect as () => Promise<void>)();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await (this.$disconnect as () => Promise<void>)();
+  }
+}
